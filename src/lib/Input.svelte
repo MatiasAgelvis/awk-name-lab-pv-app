955d524ab705819a0fba949de8ebbed747d21daa
Matias Agelvis <magelvisdoz@gmail.com>
2025-01-19 22:37:39 +0100

    Add a copy button and a clear button for the input

--- src/lib/Input.svelte
+++ src/lib/Input.svelte
@@ -1,5 +1,7 @@
 <script>
-    import { excelInput } from "../stores";
+    import { excelInput } from "../stores/variables.js";
+    import { copyToClipboard } from "../scripts/copyToClipboard.js";
+    import { createFeedbackStore } from "../stores/feedback.js";
 
     async function getClipboard() {
         try {
@@ -15,11 +17,40 @@
             $excelInput = text;
         });
     }
+
+    const CopyState = {
+        BASE: "Copy",
+        SUCCESS: "Copied",
+        ERROR: "Error",
+    };
+    $: feedback = createFeedbackStore(CopyState);
+
+    function clearInput() {
+        $excelInput = "";
+    }
 </script>
 
 <input
     type="text"
     bind:value={$excelInput}
     placeholder="Paste line from excel"
+    class="action-input"
+    class:success={$feedback === CopyState.SUCCESS}
+    class:error={$feedback === CopyState.ERROR}
 />
-<button on:click={bindClipboard}>Paste</button>
+{#if $excelInput === ""}
+    <button on:click={bindClipboard}>Paste</button>
+{:else}
+    <button
+        on:click={() => {
+            const wasCopied = copyToClipboard($excelInput);
+            feedback.show(wasCopied);
+        }}
+        class="action-button"
+        class:success={$feedback === CopyState.SUCCESS}
+        class:error={$feedback === CopyState.ERROR}
+    >
+        {$feedback}
+    </button>
+{/if}
+<button on:click={clearInput} style="background-color: red;"> Clear </button>
