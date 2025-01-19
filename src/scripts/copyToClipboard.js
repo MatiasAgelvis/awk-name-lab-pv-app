311f53e7f0c0f6065985cb3890d37f0e292b359e
Matias Agelvis <magelvisdoz@gmail.com>
2025-01-19 22:36:47 +0100

    Separation of concerns, removes feedback handling
    Will try to copy to the clipboard and return a bool depending on the success of the operation

--- src/scripts/copyToClipboard.js
+++ src/scripts/copyToClipboard.js
@@ -1,15 +1,9 @@
-export function copyToClipboard() {
-    const input = document.querySelector('.readonly-input');
-    input.select();
-    navigator.clipboard.writeText(input.value)
-        .then(() => {
-            const button = document.querySelector('.copy-button');
-            button.textContent = 'Copied!';
-            setTimeout(() => {
-                button.textContent = 'Copy';
-            }, 2000);
-        })
-        .catch(err => {
-            console.error('Failed to copy text: ', err);
-        });
+export function copyToClipboard(textToCopy) {
+  try {
+    navigator.clipboard.writeText(textToCopy);
+    return true;
+  } catch (err) {
+    console.error("Copy failed:", err);
+    return false;
+  }
 }
